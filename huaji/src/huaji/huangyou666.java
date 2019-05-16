ef00f287a2c5ba91a7e6e22c390282631df0ab9e
laobim <labim@163.com>
2019-05-16 08:55:33 +0800

    提交本次到仓库

---
+++ huaji/src/huaji/huangyou666.java
@@ -0,0 +1,12 @@
+package huaji;
+
+public class huangyou666 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			System.out.println("Hello World");            //Êä³ö¡°Hello World¡±    
+	 
+	 
+		}
+}
