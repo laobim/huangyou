9a546746d6635bb6e7068a0b46a50ee22cd9929d
laobim <49502022+laobim@users.noreply.github.com>
2019-05-30 09:22:30 +0800

    Add files via upload
    laobin

---
+++ panglebin.java
@@ -0,0 +1,23 @@
+    
+package com.nnxy.team3;
+
+public class panglebin {
+	private String name="≈”¿÷±Û";
+	private String id="20160217035";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+			
+	public void name() {
+		System.out.println(id+name);
+	}
+}
